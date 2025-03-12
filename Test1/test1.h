1badeda298dd0834be31b37f46a43a6ec4836bd3
geyulong <1412771048@qq.com>
2025-03-12 16:52:33 +0800

    修改

--- Test1/test1.h
+++ Test1/test1.h
@@ -1,8 +1,8 @@
 #pragma once
 //这一段用于动态库代码跨平台，vs/linux
 #ifdef _WIN64
-	#ifdef GYL_DLL   //随便写，统一定义为ProjectName_EXPORTS
-		#define GYL_API __declspec(dllexport)	//自己定义的，统一定义为ProjectName_API
+	#ifdef GYL_DLL   //所有动态库都可以用同一个宏，如：GYL_DLL、GYL_API
+		#define GYL_API __declspec(dllexport)	
 	#else
 		#define GYL_API __declspec(dllimport)
 	#endif
