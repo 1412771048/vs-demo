7c5255ca6826d2d1bdcded9821d23ee2662b5235
geyulong <1412771048@qq.com>
2025-03-26 12:37:31 +0800

    addd

--- Test1/test1.h
+++ Test1/test1.h
@@ -1,12 +1,12 @@
 #pragma once
 //这一段用于动态库代码跨平台，vs/linux
-#ifdef _WIN64
+#if _WIN64
 	#ifdef GYL_DLL   //所有动态库都可以用同一个宏，如：GYL_DLL、GYL_API
 		#define GYL_API __declspec(dllexport)	
 	#else
 		#define GYL_API __declspec(dllimport)
 	#endif
-#else 
+#elif __linux__
 	#define GYL_API
 #endif
 
